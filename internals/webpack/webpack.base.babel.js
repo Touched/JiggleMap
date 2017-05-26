4afa2f80c33bf18668084faa0e152916151d8b21
Touched <jtouched@gmail.com>
2017-05-26 23:54:09 +0200

    Fix local SASS

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -26,11 +26,9 @@ module.exports = (options) => ({
       // they will be a part of our compilation either way.
       // So, no need for ExtractTextPlugin here.
       test: /\.css$/,
-      include: /node_modules/,
       loaders: ['style-loader', 'css-loader'],
     }, {
       test: /\.scss$/,
-      include: /node_modules/,
       loaders: ['style-loader', 'css-loader', 'sass-loader'],
     }, {
       test: /\.(eot|svg|ttf|woff|woff2)$/,
